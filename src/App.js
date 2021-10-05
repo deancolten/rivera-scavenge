83f7cdeadfa5d1dd887d744d8952b3545bd0020c
Colten Dean <coltenrdean@gmail.com>
2021-10-05 06:36:44 -0500

    init

--- src/App.js
+++ src/App.js
@@ -1,23 +1,100 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { useCallback, useEffect, useRef, useState } from "react";
+import { pageArray } from "./pages";
 
 function App() {
+  const refNum = useRef(0);
+  const [pageNum, setPageNum] = useState(refNum.current);
+  const [canProceed, setCanProceed] = useState(false);
+  const [showTip, setShowTip] = useState(false);
+  const [inputField, setInputField] = useState("");
+
+  //On Mount
+  useEffect(() => {
+    const startingValue =
+      Number(localStorage.getItem("page")) >= 0
+        ? Number(localStorage.getItem("page"))
+        : 0;
+    refNum.current = startingValue;
+    setPageNum(refNum.current);
+  }, []);
+
+  //On Page Turn
+  useEffect(() => {
+    setCanProceed(!pageArray[pageNum].input);
+    setInputField("");
+    setShowTip(false);
+  }, [pageNum]);
+
+  const nextPage = useCallback(() => {
+    localStorage.setItem("page", refNum.current + 1);
+    refNum.current += 1;
+    setPageNum((currentPage) => currentPage + 1);
+  }, []);
+
+  const prevPage = useCallback(() => {
+    localStorage.setItem("page", refNum.current - 1);
+    refNum.current -= 1;
+    setPageNum((currentPage) => currentPage - 1);
+  }, []);
+
+  const checkInput = useCallback(
+    (e) => {
+      setInputField(e.target.value);
+      if (e.target.value.toLowerCase() === pageArray[pageNum].input) {
+        setCanProceed(true);
+      }
+    },
+    [pageNum]
+  );
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    <div className="App Quest">
+      <h2>{pageArray[pageNum].title}</h2>
+
+      <p className="QBody">{pageArray[pageNum].body}</p>
+
+      <p>
+        <span hidden={!showTip}>{pageArray[pageNum].tip}</span>
+
+        <p
+          className="hint-btn"
+          hidden={!pageArray[pageNum].tip}
+          onClick={() => {
+            setShowTip((currentTipState) => !currentTipState);
+          }}
         >
-          Learn React
-        </a>
-      </header>
+          {showTip ? "Hide Hint" : "Need Help?"}
+        </p>
+      </p>
+
+      <img
+        src={pageArray[pageNum].image}
+        className="photo"
+        hidden={!pageArray[pageNum].image}
+        alt={"Thing"}
+      />
+
+      <input
+        className="inputField"
+        hidden={!pageArray[pageNum].input}
+        onChange={checkInput}
+        value={inputField}
+      />
+      <div className="btnbox">
+        <button hidden={pageNum === 0} onClick={prevPage} className="btn">
+          Go Back
+        </button>
+
+        <button
+          hidden={pageNum === pageArray.length - 1}
+          disabled={!canProceed}
+          onClick={nextPage}
+          className="btn"
+        >
+          Continue
+        </button>
+      </div>
     </div>
   );
 }
---
+++ src/pages.js
@@ -0,0 +1,366 @@
+export const pageArray = [
+  //OPEN
+  {
+    title: "Your Quest Begins",
+    body: `Greetings adventurer! You've just begun your birthday quest. After a long and hard journey, you've arrived in the quaint but mystical town of Rivera. 
+    Once here, you decide it might be best to stop at the local 'tavern' to rest and collect your thoughts. You'll be able to tell where to go by finding a quest marker like the picture below.
+     Click 'Continue' once you've taken a seat at the bar. Click the 'Need Help' button below for tips, and if you really get stuck, just call your husband.
+    `,
+    image: "Marker.png",
+    input: null,
+    tip: `This is going to be a fun little adventure game with quests, riddles, and presents! For now, just go to the kitchen bar...`,
+  },
+
+  {
+    title: "The Tavern",
+    body: `Now that you've arrived, you have some time to clear your head and remember why you've come here. You come from a clan of Elves living in the Highlands. There is a legend in your clan of an old treasure
+    that was lost many years ago. There are rumors that the treasure can only be found when a young woman turns exactly 26 years of age. 
+    Many members of your clan have tried to find the treasure, but none have succeeded. Most have never even returned.  
+    `,
+    input: null,
+    tip: null,
+  },
+
+  {
+    title: "The Tavern",
+    body: `
+    There have been rumors circulating of a strange power that emanates from an ancient temple outside the town of Rivera. Could this have something to do with the lost treasure?
+    You ask the bartender about the temple, and he explains that the temple doors have been sealed for centuries. No one in the town seems to know who sealed the temple or how. The only person who might have the slightest clue
+    would be the Arch Mage, but his study is deep within the town's Keep, and the local guards aren't too keen of Highlander Elves.  
+    `,
+    input: null,
+    tip: null,
+  },
+
+  {
+    title: "The Tavern",
+    body: `
+    You might be able to bribe the guard at the gate, but you don't have any local currency. "I might have a job for ye...", says the bartender. 
+    "We've had a significant rat problem in the 'cellar' where we keep our food and ale. 
+    The damn things are driving most of my patrons away. I could toss some gold your way if you solve my rodent problem!"`,
+    input: null,
+    tip: null,
+  },
+  // QUEST: THE RATS
+  {
+    title: "QUEST: THE RATS",
+    body: `"Rats in the cellar... typical."`,
+    input: null,
+    tip: null,
+  },
+
+  {
+    title: "The Rats",
+    body: `You head to the cellar to deal with the rats. After a disgusting and difficult fight, you manage to kill all of the rats! How many rats were there?`,
+    input: "6",
+    tip: "If you can't find the cellar, just go to where we keep our food.",
+  },
+
+  {
+    title: "QUEST COMPLETE",
+    body: "Return to the tavern to collect your reward.",
+    input: null,
+    tip: null,
+  },
+
+  {
+    title: "The Tavern",
+    body: `Once back at the tavern, the bartender tells you to take the bag of 'gold' from the bar. 
+    He tells you that you can use that bag as an inventory for the rest of your adventure if you'd like.
+    Every adventurer needs an inventory, right? `,
+    input: null,
+    tip: null,
+  },
+
+  {
+    title: "The Keep",
+    body: `He then instructs you to head north through the large archway. There you'll find the Town Keep. Find the quest marker and enter the passphrase to bribe the guard and continue...
+    `,
+    image: "Marker.png",
+    input: "bribe",
+    tip: "Look for the marker like the image below. The code is written on the back.",
+  },
+
+  {
+    title: "The Keep",
+    body: `You did it! Continue looking for these quest markers as you progress though your journey.`,
+    image: "Marker.png",
+    input: null,
+    tip: null,
+  },
+
+  {
+    title: "The Keep",
+    body: `As you enter the keep, you’re stopped by the Guard Captain. 
+    He’s clearly not happy to see you within the walls of the keep. 
+    When you request to see the Arch Mage, the captain informs you that neither the mage nor himself have time to help you in your search. 
+    `,
+    input: null,
+    tip: null,
+  },
+
+  {
+    title: "The Keep",
+    body: `They are currently dealing with a spider problem. A GIANT spider problem. 
+    He explains that a massive cave spider has made a home within the walls of the town’s crypt, 
+    and has been causing a great deal of trouble for the citizens.`,
+    input: null,
+    tip: null,
+  },
+
+  {
+    title: "The Keep",
+    body: `The captain laughs when you offer your services to help kill the nuisance, 
+    but after he realizes you’re serious, he instructs you to at least stop by the armory and properly equip yourself. 
+    Head to the ‘armory’.
+    `,
+    input: null,
+    tip: `Just go to where you would normally get dressed`,
+  },
+
+  {
+    title: "The Keep",
+    body: `In the armory, you find some boots, leggings, and a breastplate. 
+    You decide to pass on a helmet though, because otherwise no one can see your hair. 
+    You also find a very odd triangular shaped sword. You've never seen such craftsmanship! You equip the sword and scabbard around your waste.`,
+    input: null,
+    tip: null,
+  },
+
+  {
+    title: "The Keep",
+    body: `Take some time to get geared up for the rest of your adventure. You can pick out whatever ‘armor’ is most comfortable.`,
+    input: null,
+    tip: null,
+  },
+
+  {
+    title: "QUEST: THE SPIDER",
+    body: `"At least it's not a snake...?"`,
+    input: null,
+    tip: null,
+  },
+
+  {
+    title: "The Spider",
+    body: `You need to make your way to the entrance of the crypts. 
+    You passed it when you first entered Rivera. 
+    You proceed to the entrance, but don't enter just yet..`,
+    input: null,
+    tip: `It's the hottest, darkest, and usually smelliest room in the house. And you park your car there.`,
+  },
+
+  {
+    title: "The Spider",
+    body: `You prepare yourself before entering.. You are confident in your abilities, 
+    but you’ve never taken on a beast quite like this before. 
+    You take one more breath to calm your nerves and focus before proceeding. You slowly enter the dark crypts… `,
+    input: null,
+    tip: null,
+  },
+
+  {
+    title: "The Spider",
+    body: `Find the quest marker code.`,
+    input: "legs",
+    tip: null,
+  },
+
+  {
+    title: "QUEST COMPLETE",
+    body: `Wow! That was quick! You are surprised with how easy it was to take down the massive creature. 
+    Maybe it was your new sword..? Nevertheless, you begin your journey 
+    back to the keep to tell the captain of your victory, 
+    and hopefully get answers from the Arch Mage. `,
+    input: null,
+    tip: null,
+  },
+
+  {
+    title: "The Keep",
+    body: `You return to the captain in the keep and relay the news of your triumph. 
+    He struggles to believe you, but shortly after you arrive, some of his men approach 
+    the captain and confirm the news. The captain has no choice but to point you in the direction of 
+    the Arch Mage’s quarters which are located in the south east corner of the keep by a 'very large chair'. 
+    `,
+    input: null,
+    tip: null,
+  },
+
+  {
+    title: "The Keep",
+    body: `You approach the Arch Mage and ask for any information about the mysterious temple. 
+    Much to your surprise, he is delighted by your entrigue. “The ‘Temple of Eciffo’”, he explains, 
+    “has been a major part of my study for the last 30 or so years. With the temple being sealed for so long, 
+    most people have lost interest or forgotten about it all together. With the strange power emanating from it, 
+    more people have taken notice though. However, 
+    I’m the only person who knows how to break the seal...”`,
+    input: null,
+    tip: null,
+  },
+
+  {
+    title: "QUEST: THE ARTIFACT",
+    body: `"When in doubt, go to the library."`,
+    input: null,
+    tip: null,
+  },
+  {
+    title: "The Artifact",
+    body: `The mage explains that the temple is sealed by a two word passphrase. 
+    The two words of the passphrase are inscribed on stones which are guarded by the two dragons of the 
+    Desolate Wastes to the east of town. The white dragon is much larger and intimidating, 
+    but the mage informs you that the real threat is the small gray dragon. 
+    While not nearly as large in stature, his piercing screams have been known to bring even the 
+    fiercest warriors to their knees.
+    `,
+    input: null,
+    tip: null,
+  },
+
+  {
+    title: "The Artifact",
+    body: `The Arch Mage knows of a book in the town library which he believes contains a solution to protect you from the 
+    piercing screams of the gray dragon, but due to a restraining order that he preferred not to 
+    explain in detail, the mage can’t go to the library himself. 
+    He doesn’t know the title, but gives you a riddle that he believes will lead you to it:  
+    "Though it is serious, it's a funny read. The cover is the color that dying men bleed."`,
+    input: null,
+    tip: null,
+  },
+
+  {
+    title: "The Artifact",
+    body: `Before you leave, the mage instructs you to take some rejuvenation potions from the top drawer of his tall black chest. 
+    “You may not need or be able to use these now, but there will be a time in the future that these 
+    might just come in handy.”`,
+    input: null,
+    tip: null,
+  },
+
+  {
+    title: "The Artifact",
+    body: ` 
+    You head to the library and try to locate the ancient text...`,
+    input: "page",
+    tip: "Go to the bookshelf and look for a new red book",
+  },
+
+  {
+    title: "The Artifact",
+    body: `After reading the text, you learn that there is an ancient dwarven artifact that can 
+    drown out the screams of the dragon. 
+    You also conveniently find an old map that leads to the location of the artifact: a hidden cave to the west of town.
+    You head out in search of the cave.`,
+    input: null,
+    tip: null,
+  },
+
+  {
+    title: "The Artifact",
+    body: `You find the entrance of a dark, smelly cave. It’s clear that some type of troll spends a lot of time here,
+     but it seems like he’s not here today. You look around until you find the artifact in one of the troll's chests. 
+    `,
+    input: "ears",
+    tip: null,
+  },
+
+  {
+    title: "The Artifact",
+    body: `You take the two strange devices out of their case, just to make sure they work. 
+    You also find a strange symbol. It might be useful.`,
+    input: null,
+    tip: null,
+  },
+
+  {
+    title: "QUEST COMPLETE",
+    body: `With the artifacts in ..hand? ...ear?, you return to the Arch Mage to receive more instruction.`,
+    input: null,
+    tip: null,
+  },
+
+  {
+    title: "The Keep",
+    body: `The mage is enthralled by your recent find, and although he clearly wants to take time 
+    to study the ancient devices, you insist that you need to continue with your journey.`,
+    input: null,
+    tip: null,
+  },
+
+  {
+    title: "The Keep",
+    body: `
+    “The time has come,” he quietly says. 
+    “You have essentially all that you need to face the dragons. 
+    However, I might have one more gift for you.”`,
+    input: null,
+    tip: null,
+  },
+
+  {
+    title: "The Keep",
+    body: `He instructs you to open the bottom drawer of his chest. 
+    “You might be travelling alone now, but soon you may find that it’s better to have a companion on your journey. 
+    This special garment of mine might be helpful when the time comes.”`,
+    input: null,
+    tip: null,
+  },
+
+  {
+    title: "The Keep",
+    body: `After recieving your final gift from the mage, you head out toward the Desolate Wastes to face your final trial.`,
+    input: null,
+    tip: null,
+  },
+
+  {
+    title: "QUEST: ECIFFO AND THE DRAGONS",
+    body: `"This might get ruff.."`,
+    input: null,
+    tip: null,
+  },
+
+  {
+    title: "Eciffo and the Dragons",
+    body: `You head to the edge of town toward the desolate wastes. In the distance, you spot the gray and white dragons. 
+    You can already hear the terrifying screams… You put in your ancient ear devices, and charge into battle!`,
+    input: null,
+    tip: null,
+  },
+
+  {
+    title: "Eciffo and the Dragons",
+    body: `In a fight only slightly more difficult than your tangle with the cave spider, 
+    you manage to subdue the two majestic beasts and acquire the two inscriptions around their necks.`,
+    input: null,
+    tip: null,
+  },
+
+  {
+    title: "Eciffo and the Dragons",
+    body: `Now that you have the passphrase, 
+    you have everything you need to enter the Temple of Eciffo. 
+    You journey to the temple door and speak the phrase..`,
+    input: "good boy",
+    tip: `I am aware that it is very likely one or both of the of the dogs ripped off the quest markers. 
+    You've played along this far, so the code is 'good boy'`,
+  },
+
+  {
+    title: "QUEST COMPLETE",
+    body: `You enter the temple and find what you've been looking for: THE TREASURE! IT'S REAL!! 
+    ..and for some odd reason you get the feeling it's even better than your husband's..`,
+    input: null,
+    tip: null,
+  },
+
+  {
+    title: "The End Of The Road",
+    body: `With aching feet, high spirits, and a bag full of treasure, 
+    you make your way back to the tavern to drink and celebrate! 
+    Thank you for playing along, lass! I hope you enjoyed a unique birthday celebration!`,
+    input: null,
+    tip: "Just go to the dang kitchen. I'm gonna sing you a song.",
+  },
+];
